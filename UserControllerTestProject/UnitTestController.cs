6f0650b2ba51986c0ee59c193455e408a5f05eeb
Mohammed El Alladi <moal@eaaa.dk>
2026-03-03 10:53:38 +0100

    addition

--- UserControllerTestProject/UnitTestController.cs
+++ UserControllerTestProject/UnitTestController.cs
@@ -7,10 +7,12 @@ namespace UserControllerTestProject
         public void TestMethod1()
         {
             // Arrange
-
+                int a = 5;
+                int b = 10;
             // Act
-
-            //
+            int sum = a + b;
+            // Assert
+            Assert.AreEqual(15, sum);
         }
     }
 }
